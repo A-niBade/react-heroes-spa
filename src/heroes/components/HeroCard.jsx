8f03676c620445dfe9710a6f71c913fa3c1fe234
Anibal <joan.maldonadoa@gmail.com>
2022-08-12 14:54:37 -0400

    Search implemented

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -12,7 +12,7 @@ export const HeroCard = ({
   first_appearance,
   characters,
 }) => {
-  const heroImageUrl = `/assets/heroes/${id}.jpg`;
+  const heroImageUrl = `../assets/heroes/${id}.jpg`;
 
   return (
     <div className="col">
