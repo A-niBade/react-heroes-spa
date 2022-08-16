3f2615be445da1cb6706b3638e70c9c6c99552de
Anibal <joan.maldonadoa@gmail.com>
2022-08-16 13:58:57 -0400

    Fixed typo

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -15,49 +15,52 @@ export const Navbar = () => {
   };
 
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
-      <Link className="navbar-brand" to="/">
-        Asociaciones
-      </Link>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-2">
+      <div className="collapse navbar-collapse">
+        <div className="navbar-collapse">
+          <div className="navbar-nav">
+            <Link className="navbar-brand" to="/">
+              Associations
+            </Link>
+            <NavLink
+              className={({ isActive }) =>
+                `nav-item nav-link ${isActive ? "active" : ""}`
+              }
+              to="/marvel"
+            >
+              Marvel
+            </NavLink>
 
-      <div className="navbar-collapse">
-        <div className="navbar-nav">
-          <NavLink
-            className={({ isActive }) =>
-              `nav-item nav-link ${isActive ? "active" : ""}`
-            }
-            to="/marvel"
-          >
-            Marvel
-          </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                `nav-item nav-link ${isActive ? "active" : ""}`
+              }
+              to="/dc"
+            >
+              DC
+            </NavLink>
 
-          <NavLink
-            className={({ isActive }) =>
-              `nav-item nav-link ${isActive ? "active" : ""}`
-            }
-            to="/dc"
-          >
-            DC
-          </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                `nav-item nav-link ${isActive ? "active" : ""}`
+              }
+              to="/search"
+            >
+              Search
+            </NavLink>
 
-          <NavLink
-            className={({ isActive }) =>
-              `nav-item nav-link ${isActive ? "active" : ""}`
-            }
-            to="/search"
-          >
-            Search
-          </NavLink>
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+              <ul className="navbar-nav ml-auto">
+                <span className="nav-item nav-link text-primary">
+                  {user?.name}
+                </span>
+                <button className="nav-item nav-link btn" onClick={onLogout}>
+                  Logout
+                </button>
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
-        <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-primary">{user?.name}</span>
-          <button className="nav-item nav-link btn" onClick={onLogout}>
-            Logout
-          </button>
-        </ul>
       </div>
     </nav>
   );
