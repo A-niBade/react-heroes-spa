fb99e08d228217696cbcc67d7ae27095d834a17a
Anibal <joan.maldonadoa@gmail.com>
2022-08-16 17:26:35 -0400

    HeroCard styles done

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 
+import "./styles/HeroCard.css";
+
 const CharacterByHero = ({ alter_ego, characters }) => {
   return alter_ego === characters ? <></> : <p>{characters}</p>;
 };
@@ -15,33 +17,27 @@ export const HeroCard = ({
   const heroImageUrl = `../assets/heroes/${id}.jpg`;
 
   return (
-    <div className="col">
-      <div className="card">
-        <div className="row no-gutters">
-          <div className="col-4">
-            <img
-              src={heroImageUrl}
-              className="card-img animate__animated animate__flipInY"
-              alt={superhero}
-            ></img>
-          </div>
-
-          <div className="col-8">
-            <div className="card-body">
-              <h5 className="card-title">{superhero}</h5>
-              <p className="card-text">{alter_ego}</p>
-
-              <CharacterByHero characters={characters} alter_ego={alter_ego} />
-
-              <p className="card-text">
-                <small className="text-muted">{first_appearance}</small>
+    <Link
+      to={`/hero/${id}`}
+      className="hero-pic animate__animated animate__fadeIn"
+    >
+      <img src={heroImageUrl} className="img img-responsive" alt={superhero} />
+      <div className="hero-pic__name">{superhero}</div>
+      <div className="hero-pic__alter">{alter_ego}</div>
+      <div className="hero-pic__profile">
+        <div className="hero-pic__profile">
+          <div className="row">
+            <div className="col-ms-4">
+              <h4>{publisher}</h4>
+              <p className="fw-bold">
+                First appaerance: <br />
+                {first_appearance}
               </p>
-
-              <Link to={`/hero/${id}`}>Más...</Link>
+              {alter_ego !== characters && <p>{characters}</p>}
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
